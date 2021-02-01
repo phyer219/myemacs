5d07adf8481af2a6f136866534a714fe10c56ac5
xps13 <phyer219@gmail.com>
2021-02-01 15:50:19 +0800

    update init-python

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,5 +1,8 @@
+;; 选择用哪一个 python 解释器
+;; (setq python-shell-interpreter "/opt/anaconda/bin/python3.7")
+
 ;; python 插件
 (require-package 'elpy)
-;;(elpy-enable)
+(elpy-enable)
 ;;-----------------------------------------------------------------------------
 (provide 'init-python)
