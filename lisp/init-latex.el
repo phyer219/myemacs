3a8b48b788a427d566684092b07354e2a7c113da
xps13 <phyer219@gmail.com>
2021-02-01 22:46:56 +0800

    删除 C-c c 快速 latex 编译快捷键

--- lisp/init-latex.el
+++ lisp/init-latex.el
@@ -17,12 +17,12 @@
 
 
 ;; 小技巧 M-x describe-key <RET> C-c C-c 可以查看快捷键对应的命令
-(defun quick-latex ()
-  "A quick latex"
-  (interactive)
-  (save-buffer)
-  (TeX-command-menu "LaTeX"))
-;;Short key F9
-(global-set-key [(control c) (c)] 'quick-latex)
+;; (defun quick-latex ()
+;;   "A quick latex"
+;;   (interactive)
+;;   (save-buffer)
+;;   (TeX-command-menu "LaTeX"))
+;; ;;Short key F9
+;; (global-set-key [(control c) (c)] 'quick-latex)
 
 (provide 'init-latex)
