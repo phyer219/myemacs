f151f25c9b4cb7a623c180c953cb0c5d3c431cb3
xps13 <phyer219@gmail.com>
2021-02-01 23:01:52 +0800

    debug

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -63,19 +63,12 @@
 ;; (setq-default which-key-idle-delay 1.5)
 ;; (with-eval-after-load 'which-key
 ;; (diminish 'which-key-mode))
-;;------------------------------------------------------------------------------
-;; 搜索的插件
-;; (require-package 'swiper)
-;; ;; swiper 需要先启动 ivy-mode. 上面已经开启.
-;; ;; 配置参考自: https://www.youtube.com/watch?v=AaUlOH4GTCs
-;; (global-set-key "\C-s" 'swiper)
-;;------------------------------------------------------------------------------
+
 ;;-----------------------------------------------------------------------------
 ;; 语法检查
 (require-package 'flycheck)
 ;; 打开 flychek-mode 关了, 感觉不好用
 ;;(global-flycheck-mode 1)
 ;;-----------------------------------------------------------------------------
-
 
 (provide 'init-editing-utils)
