ee631fe19edfc94e579d90fb63c0cbc5aa084725
xps13 <phyer219@gmail.com>
2021-02-02 12:04:17 +0800

    添加 scratch 显示的信息

--- lisp/init-show-message.el
+++ lisp/init-show-message.el
@@ -1,3 +1,7 @@
+;; scratch buffer 显示的信息, 参考:
+;; https://stackoverflow.com/questions/1498258/how-do-i-change-the-scratch-message-in-emacs/1498292
+(setq initial-scratch-message ";; Emacs ❤ you!\n")
+
 ;; 修改启动时 minibuffer 显示的信息. 参考:
 ;; https://emacs.stackexchange.com/questions/432/how-to-change-default-minibuffer-message
 (defun display-startup-echo-area-message ()
