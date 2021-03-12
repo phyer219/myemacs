25d0737c12754f2d9ca8c231f39e7d85e0092d0b
xps13 <phyer219@gmail.com>
2021-03-12 14:38:18 +0800

    display ruler in org-mode

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -56,6 +56,7 @@
 ;;(setq-default indicate-buffer-boundaries 'left)
 ;;(setq-default display-fill-column-indicator-character ?\u254e)
 (add-hook 'prog-mode-hook 'display-fill-column-indicator-mode)
+(add-hook 'org-mode-hook 'display-fill-column-indicator-mode)
 
 ;; 方案2:
 ;; (require-package 'fill-column-indicator)
