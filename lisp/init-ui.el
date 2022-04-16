e535852d96b2cd2f196c004fddbe50e81226d696
zqw_archnuc <phyer219@gmail.com>
2022-04-16 14:15:35 +0800

    forget what i did...

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -70,4 +70,18 @@
 ;; lsp 更好的ui
 ;; (require-package 'lsp-ui) 有点乱, 还是不要了
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Hack Nerd Font Mono" :foundry "SRC" :slant normal :weight normal :height 143 :width normal)))))
+
 (provide 'init-ui)
